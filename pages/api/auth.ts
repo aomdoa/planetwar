1914502822d9699ea33776d5e4ee49ccf9d18aec
David Shurgold <aomdoa@gmail.com>
2020-06-07 19:45:40 -0400

    Completed the basic user management and login

---
+++ pages/api/auth.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client"
+import { successMessage, failMessage, getCurrentUser } from './shared'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { APP_SECRET } from '../config'
+import { getUnpackedSettings } from "http2"
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn'],
+})
+
+/**
+ * Only the POST method to login user
+ * POST login with the email and password
+ * @param req Standard request parameter for prisma
+ * @param res Standard result parameter for prisma
+ * @return res.json {success: BOOLEAN, data: { token: TOKEN, user: DATA}}
+ */
+export default async function handle(req, res) {
+  console.log(`auth.ts`)
+  if (req.method === "POST") {
+      console.log(`POST ${JSON.stringify(req.body)}`)
+      
+      const user = await prisma.user.findOne({
+        where: { email: String(req.body.email) },
+      }).catch(err => {
+        return failMessage(res, err.message)
+      })
+
+      if (!user) {
+          return failMessage(res, `Unable to find a user with that email '${req.body.email}' and password '${req.body.password}'`)
+      }
+   
+      console.log(`Maybe logging in user ${JSON.stringify(user)}`)
+
+      const valid = await bcrypt.compare(String(req.body.password), user.password)
+      if (!valid) {
+        return failMessage(res, `Unable to find a user with that email '${req.body.email}' and password '${req.body.password}'`)
+      }
+      delete user.password
+
+      const token = jwt.sign(user, APP_SECRET)
+      const returnData = { token: token, user: { id: user.id, name: user.name, email: user.email }}
+      console.log(`Returning ${JSON.stringify(returnData)}`)
+      return successMessage(res, returnData)
+  } else {
+    return failMessage(res, `Unsupported request type ${req.method}`)
+  }
+}
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,50 +1,83 @@
 import { PrismaClient } from "@prisma/client"
-import { successMessage, failMessage } from './shared'
+import { successMessage, failMessage, getCurrentUser } from './shared'
+import bcrypt from 'bcrypt'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn'],
+})
+
+const selectFields = { id: true, name: true, email: true }
 
 /**
- * Manage the incoming request and direction to the correct underlying message based on the parameter passed through ?
+ * Manage the incoming request and direction to the correct underlying message based on the parameter passed through
+ * GET user?id=NUM - handleGetOne
+ * GET user - handleGetList
+ * PUT user?id=NUM - handleUpdate - {name, email, password}
+ * POST user - handleCreate - {name, email, password}
  * @todo Fix the error processing by maybe doing the parent catch
  * @param req Standard request parameter for prisma
  * @param res Standard result parameter for prisma
+ * @return res.json {success: BOOLEAN, data: DATA}
  */
 export default async function handle(req, res) {
   console.log(`user.ts handle`)
+  if (req.method === "POST") {
+    return handleCreate(req.body, res)
+  }
+
+  const user = getCurrentUser(req)
+  if (!user) {
+    return failMessage(res, `User must be authenticated`)
+  }
+
+  const userId = Number(req.query.id)
   if (req.method === "GET") {
-    const userId = req.query.id
     if (userId) {
       return handleGetOne(userId, res)
     } else {
       return handleGetList(res)
     }
-  } else if (req.method === "POST") {
-      console.log(`POST`)
+  } else if (req.method === "PUT") {
+    if (user.id !== userId) {
+      return failMessage(res, `Unable to update other user account`)
+    } else {
+      return handleUpdate(userId, req.body, res)
+    }
   } else {
     return failMessage(res, `Unsupported request type ${req.method}`)
   }
 }
 
 /**
- * Retrieve a single user based on the req.query.id
- * Posts the resulting user data through res.json
- * @param incomingUserId The user id provided to be loaded from the database
+ * Utility method to load the user from the database
+ * @param incomingUserId The id of the user to load
  * @param res
+ * @return User User object found by id
  */
-async function handleGetOne(incomingUserId, res) {
-  console.log(`handleGetOne ${incomingUserId}`)
-  const userId = Number(incomingUserId)
+async function selectOneUser(userId, res) {
+  console.log(`selectOneUser userId: ${userId}`)
   if (!(userId > 0)) {
-    return failMessage(res, `Could not find user with id ${incomingUserId}`)
+    return failMessage(res, `Invalid user id: ${userId}`)
   }
-  
+
   const user = await prisma.user.findOne({
-    where: { id: Number(userId) },
+    select: selectFields,
+    where: { id: userId },
   }).catch(err => {
     return failMessage(res, err.message)
   })
   console.dir(user)
+  return user
+}
 
+/**
+ * Retrieve a single user based on the userId
+ * @param userId The user id provided to be loaded from the database
+ * @param res
+ * @return res.json
+ */
+async function handleGetOne(userId, res) {
+  const user = await selectOneUser(userId, res)
   if (user) {
     return successMessage(res, user)
   } else {
@@ -52,11 +85,74 @@ async function handleGetOne(incomingUserId, res) {
   }
 }
 
+/**
+ * Retrieves the list of users
+ * @param res 
+ * @return res.json Array of users
+ */
 async function handleGetList(res) {
   console.log(`handleGetList`)
-  const users = await prisma.user.findMany()
+  const users = await prisma.user.findMany({
+    select: selectFields
+  })
   console.dir(users)
   return successMessage(res, users)
 }
 
-async function handleCreate(data, res) {}
+/**
+ * Handle the creation of a new user based on the data
+ * @param data The data to use for creation. name, email and password
+ * @param res 
+ * @return res.json
+ */
+async function handleCreate(data, res) {
+  console.log(`handleCreate ${JSON.stringify(data)}`)
+  if(!data.name) {
+    return failMessage(res, 'name must be provided for the user')
+  } else if(!data.email) {
+    return failMessage(res, 'email must be provided for the user')
+  } else if(!data.password) {
+    return failMessage(res, 'password must be provided for the user')
+  }
+  data.password = await bcrypt.hash(data.password, 10)
+
+  try {
+    const user = await prisma.user.create({
+      select: selectFields,
+      data: data
+    })
+    console.log(`returning createdUser ${JSON.stringify(user)}`)
+    return successMessage(res, user)
+  } catch(err) {
+    return failMessage(res, err.message)
+  }
+}
+
+/**
+ * Handle the update an user based on the id and data
+ * @param id The id of the user to update
+ * @param data The data to update for the user
+ * @param res 
+ * @return res.json
+ */
+async function handleUpdate(id, data, res) {
+    console.log(`handleUpdate ${id} ${JSON.stringify(data)}`)
+    const user = await selectOneUser(id, res)
+    if(data.name) {
+      user.name = data.name
+    }
+    if(data.email) {
+      user.email = data.email
+    }
+    if(data.password) {
+      user.password = await bcrypt.hash(data.password, 10)
+      console.log(`setting the password from ${data.password} to ${user.password}`)
+    }
+    const updatedUser = await prisma.user.update({
+      select: selectFields,
+      data: user,
+      where: { id: user.id },
+    })
+    console.log(`returning updatedUser ${JSON.stringify(updatedUser)}`)
+    return successMessage(res, updatedUser)
+}

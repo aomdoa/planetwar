7ac96f6daedfa82bdeb81f5703ec4e6c90441c90
David Shurgold <aomdoa@gmail.com>
2020-06-18 17:04:23 -0400

    Very basic load working!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react'
+import { GetServerSideProps } from 'next'
 import Layout from '../components/Layout'
 
-const Index : React.FC = () => {
+//TODO: Add proper type definition
+type Props = {
+  games: Object[]
+}
+
+const Index : React.FC<Props> = props => {
   return (
     <Layout>
-      <div className="page">
-        <h1>index</h1>
+      <div>
+        <h2>index</h2>
+        <p>Auth and stuff we'll get figured out later</p>
+        <main>
+        {props.games.map(game => (
+          <div key={game.id}>Game #{game.id}: {game.name}</div>
+        ))}
+        </main>
       </div>
       <style jsx>{`
       `}</style>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/games', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFvbWRvYUBnbWFpbC5jb20iLCJpZCI6MSwibmFtZSI6ImFvbWRvYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU5MTgzMTY4NX0.-uXAxUKYUXJj681WlnfDmuOQmak902tXugOAGaoDsCs'
+    }
+  })
+  const response = await res.json()
+  //TODO: Add check for success
+  return {
+    props: { 'games': response.data }
+  }
 }
 
 export default Index
